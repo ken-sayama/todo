52e30b1f944d3958d81203952717a41dc6e74250
ken-sayama <sayamaken0402@gmail.com>
2018-02-22 18:16:43 +0900

    add main view settings

--- todo/AppDelegate.swift
+++ todo/AppDelegate.swift
@@ -13,10 +13,20 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // ナビゲーションの設定
+        let viewController: ViewController = ViewController()
+        // ViewControllerを起点にする
+        let navigationController = UINavigationController(rootViewController: viewController)
+        
+        // ビューの設定
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.backgroundColor = UIColor.white
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 
