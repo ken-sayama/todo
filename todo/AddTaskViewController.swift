b3801575aef662b129cc711814c7ae4aae5b5bbe
ken-sayama <sayamaken0402@gmail.com>
2018-02-22 18:29:21 +0900

    create navigationController

---
+++ todo/AddTaskViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddTaskViewController.swift
+//  todo
+//
+//  Created by 佐山絢 on 2018/02/22.
+//  Copyright © 2018年 佐山絢. All rights reserved.
+//
+
+import UIKit
+
+class AddTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- todo/ViewController.swift
+++ todo/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // ナビゲーションバーのタイトルをセット
+        self.navigationItem.title = "Todo"
         
-        
+        // ナビゲーションバーの右上にAddという文字を表示させ、押すとAddTaskViewControllerに遷移させる
+        let addTaskBarButton = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(self.moveTaskPage))
+        self.navigationItem.rightBarButtonItem = addTaskBarButton
+    }
+    
+    // Addを押した際のページ遷移
+    @objc func moveTaskPage(){
+        // 遷移先のコントローラー
+        let controller = AddTaskViewController()
+        // 遷移の際にアニメーションで遷移する
+        self.navigationController?.pushViewController(controller, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
