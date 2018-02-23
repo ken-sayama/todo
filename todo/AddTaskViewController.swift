c4e1497e0485890c2072a709b0145b2e19a0122b
ken-sayama <sayamaken0402@gmail.com>
2018-02-23 12:20:23 +0900

    add button and textField

--- todo/AddTaskViewController.swift
+++ todo/AddTaskViewController.swift
@@ -12,24 +12,38 @@ class AddTaskViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // windowの幅と高さを取得しておく
+        let frameWidth = self.view.frame.width
+        let frameHeight = self.view.frame.height
+        
+        // ラベルの定義とレイアウト
+        let label = UILabel(frame: CGRect(x: frameWidth / 4, y: frameHeight / 2 - 50, width: 200, height: 50))
+        label.text = "What do you have task ?"
+        self.view.addSubview(label)
+        
+        // テキストフィールドの定義
+        let textArea = UITextField(frame: CGRect(x: frameWidth / 4, y: frameHeight / 2, width: 200, height: 50))
+        textArea.placeholder = "Write about your task"
+        textArea.textColor = UIColor.black
+        textArea.layer.borderWidth = 1.0
+        textArea.layer.borderColor = UIColor.gray.cgColor
+        textArea.layer.cornerRadius = 2.0
+        textArea.leftViewMode = .always
+        textArea.leftView = UIView(frame: CGRect(x:0,y:0,width:10,height:10))
+        self.view.addSubview(textArea)
+        
+        // ボタンの定義
+        let btn = UIButton(frame: CGRect(x: frameWidth / 4, y:frameHeight / 2 + 70, width: 200, height: 40))
+        btn.setTitle("Add", for: .normal)
+        btn.setTitleColor(UIColor.white, for: .normal)
+        btn.backgroundColor = UIColor.black
+        btn.layer.cornerRadius = 20.0
+        self.view.addSubview(btn)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
